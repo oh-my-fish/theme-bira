93a873e561b8e49384c957edc686397e46ec74a9
Naoaki Sato <naoaksat@yahoo-corp.jp>
2019-08-30 14:20:12 +0900

    k8s status add

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -37,6 +37,28 @@ function __git_status
   end
 end
 
+function __kubernetes_status
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
+    return
+  end
+
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
+    return
+  end
+
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
+
+  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
+end
+
 function __ruby_version
   if type "rvm-prompt" > /dev/null 2>&1
     set ruby_version (rvm-prompt i v g)
@@ -55,6 +77,7 @@ function fish_prompt
   __current_path
   __ruby_version
   __git_status
+  __kubernetes_status
   echo -e ''
   echo (set_color white)"╰─"(set_color --bold white)"\$ "(set_color normal)
 end
